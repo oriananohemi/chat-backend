b3b6edd81d6efddb590bc507002ede0c5cd8fb50
Oriana <oriana.nohemi22@gmail.com>
2020-11-11 11:44:28 -0500

    crear servidor con express y añadir ejemplos de rutas

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 
+const router = express.Router();
+
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hola");
+app.use(router);
+
+router.get("/", (req, res) => {
+  res.send("Lista de mensajes");
 });
+
+router.post("/", (req, res) => {
+  res.send("Mensaje añadido");
+});
+
+// app.use("/", (req, res) => {
+//   res.send("Hola");
+// });
 
 app.listen(3000);
 console.log("La aplicacion esta escuchando en http://localhost:3000");
