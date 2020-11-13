d1ac1dbfdb073913f478568c56796575a32d6c70
Oriana <oriana.nohemi22@gmail.com>
2020-11-13 15:59:37 -0500

    crea archivo routes para separar las responsabilidades

--- server.js
+++ server.js
@@ -1,28 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const router = express.Router();
 
-const response = require("./network/response");
+const router = require("./network/routes");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(router);
-
-router.get("/", (req, res) => {
-  res.header({
-    "custom-header": "Nuestro valor personalizado",
-  });
-  response.success(req, res, "lista de mensajes");
-});
-
-router.post("/", (req, res) => {
-  if (req.query.error === "ok") {
-    response.error(req, res, "error simulado", 400, "Error de Prueba");
-  } else {
-    response.success(req, res, "Creado Correctamente", 201);
-  }
-});
+// app.use(router);
+router(app);
 
 app.use("/app", express.static("public"));
 
