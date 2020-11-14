8e47e6c7a4b883b79da03d04933ff37f53ec85ba
Oriana <oriana.nohemi22@gmail.com>
2020-11-13 23:09:36 -0500

    agrega componente de usuarios

---
+++ components/user/network.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const response = require("../../network/response");
+const controller = require("./controller");
+const router = express.Router();
+
+router.post("/", (req, res) => {
+  controller
+    .addUser(req.body.name)
+    .then((data) => {
+      response.success(req, res, data, 201);
+    })
+    .catch((e) => {
+      response.error(req, res, "Internal Error", 500, e);
+    });
+});
+
+module.exports = router;
